93eaf0aa58e299b0c475a3f3a9f3ddb39c05e9c4
John Taggart <taggartj1985@gmail.com>
2020-04-29 18:24:43 +0100

    get,posts,put and delete working

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const createRouter = require('./createRouter.js');
+const bodyParser = require('body-parser');
+
+const dogs = [
+  { name: "Fluffy", breed: "Pomeranian" },
+  { name: "Kane", breed: "Rottweiler" },
+  { name: "Bonzo", breed: "German Shepherd" },
+  { name: "Lucky", breed: "Cockapoo" },
+  { name: "Luna", breed: "Husky" }
+];
+
 app.use(cors());
+app.use(bodyParser.json());
+const dogsRouter = createRouter(dogs);
 
 
-//`/` is the root route
-// we're Not saying do a get.
-// we're actually saying listen for a get from the client(s)
-app.get('/', function(req, res){
-  // when a Get is fetched on / - the the code in this function is triggered.
-  // We get the request and response objects to use,
-  // to help us make a decision about what to do.
-  res.json({message: "Hi World"})
+app.use('/api/dogs/', dogsRouter)
+
+
+app.listen(3000, function () {
+  console.log(`App running on port ${ this.address().port }`);
 });
-
-
-app.listen(3000, function(){
-  console.log('App running on port 3000');
-})
